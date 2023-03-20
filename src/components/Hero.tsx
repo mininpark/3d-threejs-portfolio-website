395a47af855c27a735166f28f30db0df146fa504
mininpark <hunmin.park@web.de>
2023-03-20 16:58:35 +0100

    added section Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="section-hero">
+      <div className="section-hero__wrapper paddingX">
+        <div className="section-hero__shapes">
+          <div className="section-hero__shapes-circle" />
+
+          <div className="section-hero__shapes-line violet-gradient" />
+        </div>
+        <div>
+          <h1 className="heroHeadText font-white">Hi, I'm <span>Mina</span></h1>
+          <p className="heroSubText font-white">I develop 3D visuals, User <br className="dynamic-br" />interfaces and web applications</p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+    </section>
   )
 }
 
